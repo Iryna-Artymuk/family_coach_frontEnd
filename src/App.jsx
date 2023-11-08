3aee321963b05e8eb80292a2f73cd01f720dfe37
Iryna Artymuk <artimyk@ukr.net>
2023-11-08 15:24:26 +0200

    feat(add base structure  to new  repo): add base structure

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import HomePage from './pages/main/HomePage/HomePage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          {/* <Route path="/posters" element={<PostersPage/>} />
+          <Route path="*" element={<Page404 />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/gallery" element={<Gallery />} /> */}
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/main/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import Container from '@/components/Container/Container';
+import style from './HomePage.module.scss';
+const HomePage = () => {
+  return (
+    <Container>
+      <p className={style.test}>family_coach </p>
+      <p className={style.test}>Жанна Барищук </p>
+    </Container>
+  );
+};
+
+export default HomePage;
