d79f27040c4f1f8d784f964586e65dadfd349afb
Iryna Artymuk <artimyk@ukr.net>
2023-11-09 20:00:38 +0200

    fix(app): fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
+
 import HomePage from './pages/main/HomePage/HomePage';
 import BlogPage from './pages/main/BlogPage/BlogPage';
+import Layout from './components/Layout/Layout';
 
 const App = () => {
   return (
