e04687085de0445b233436b9e4a14b629d222f94
Iryna Artymuk <artimyk@ukr.net>
2023-11-11 16:05:38 +0200

    feat(homepage): feat hero section

--- src/pages/main/HomePage/HomePage.jsx
+++ src/pages/main/HomePage/HomePage.jsx
@@ -1,9 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import style from './HomePage.module.scss';
 
 import Container from '@/components/Container/Container';
 
 const HomePage = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -11,19 +24,32 @@ const HomePage = () => {
     <Container>
       <section id="about" className={style.hero}>
         <div className={style.contentWrapper}>
-          <div className={style.heroImage}>Фото</div>
+          {windowWidth >= 768 && (
+            <div className={style.heroImage}>
+              <div className={style.quote}>
+                <p>Як мало нам потрібно для щастя - трохи знань про себе.</p>
+              </div>
+            </div>
+          )}
           <div className={style.about}>
-            <h1 className={style.aboutName}>
-              Привіт!
-              <br />
-              Мене звати Жанна Барищук
-            </h1>
+            
+            <p className={ style.aboutName }>Привіт!<br/>
+              Мене звати Жанна Барищук</p>
             <h2 className={style.aboutJob}>Практикуючий психолог</h2>
             <h2 className={style.aboutContent}>
               Тут про любов до себе, здорові сімейні стосунки та виховання
               щасливих дітей♡
             </h2>
           </div>
+          {windowWidth < 768 && (
+            <div className={style.heroImage}>
+              <div className={style.heroImage}>
+                <div className={style.quote}>
+                  <p>Як мало нам потрібно для щастя - трохи знань про себе.</p>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </section>
 
