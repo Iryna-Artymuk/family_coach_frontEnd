f4a5b3b811da10b9e9ec65f7daa9318f238f4d8a
Iryna Artymuk <artimyk@ukr.net>
2023-11-12 15:28:13 +0200

    feat(home page): add section why me html css

--- src/pages/main/HomePage/HomePage.jsx
+++ src/pages/main/HomePage/HomePage.jsx
@@ -2,60 +2,96 @@ import { useEffect, useState } from 'react';
 import style from './HomePage.module.scss';
 
 import Container from '@/components/Container/Container';
+import { useMediaQuery } from 'react-responsive';
 
 const HomePage = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth);
-    }
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <Container>
+    <>
       <section id="about" className={style.hero}>
-        <div className={style.contentWrapper}>
-          {windowWidth >= 768 && (
-            <div className={style.heroImage}>
-              <div className={style.quote}>
-                <p>Як мало нам потрібно для щастя - трохи знань про себе.</p>
-              </div>
-            </div>
-          )}
-          <div className={style.about}>
-            
-            <p className={ style.aboutName }>Привіт!<br/>
-              Мене звати Жанна Барищук</p>
-            <h2 className={style.aboutJob}>Практикуючий психолог</h2>
-            <h2 className={style.aboutContent}>
-              Тут про любов до себе, здорові сімейні стосунки та виховання
-              щасливих дітей♡
-            </h2>
-          </div>
-          {windowWidth < 768 && (
-            <div className={style.heroImage}>
+        <Container>
+          <div className={style.contentWrapper}>
+            {isDesktop && (
               <div className={style.heroImage}>
                 <div className={style.quote}>
                   <p>Як мало нам потрібно для щастя - трохи знань про себе.</p>
                 </div>
               </div>
+            )}
+            <div className={style.about}>
+              <p className={style.aboutName}>
+                Привіт!
+                <br />
+                Мене звати Жанна Барищук
+              </p>
+              <h2 className={style.aboutJob}>Практикуючий психолог</h2>
+              <h2 className={style.aboutContent}>
+                Тут про любов до себе, здорові сімейні стосунки та виховання
+                щасливих дітей♡
+              </h2>
             </div>
-          )}
-        </div>
+            {isMobile && (
+              <div className={style.heroImage}>
+                <div className={style.heroImage}>
+                  <div className={style.quote}>
+                    <p>
+                      Як мало нам потрібно для щастя - трохи знань про себе.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </Container>
       </section>
+      <section className={style.whyMe}>
+        <Container>
+          <div className={style.whyMe_contentWrapper}>
+            {isDesktop && (
+              <>
+                <div className={style.whyMeContent}>
+                  <h2 className={style.whyMeTitle}>В чому моя унікальність?</h2>
+                  <p>
+                    Я реалізована особистість. Я кваліфікований спеціаліст,
+                    кохана, мама, дочка, сестра та подруга. Працюю в морі, і
+                    будучи 50/50 свого часу та, я не втратила контакт зі своїм
+                    оточенням. Я знаю, що таке баланс між особистим життям,
+                    саморозвитком, роботою та відпочинком.
+                  </p>
+                </div>
+                <div className={style.whyMeImg}></div>
+              </>
+            )}
+            {isMobile && (
+              <>
+                <h2 className={style.whyMeTitle}>В чому моя унікальність?</h2>
 
+                <div className={style.whyMeImgWrapper}>
+                  <div className={style.whyMeImg}></div>
+                  <p className={style.whyMeContent}>
+                    Я реалізована особистість. Я кваліфікований спеціаліст,
+                    кохана, мама, дочка, сестра та подруга.
+                  </p>
+                </div>
+
+                <p className={style.whyMeContentSecondText}>
+                  Працюю в морі, і будучи 50/50 свого часу та, я не втратила
+                  контакт зі своїм оточенням. Я знаю, що таке баланс між
+                  особистим життям, саморозвитком, роботою та відпочинком.
+                </p>
+              </>
+            )}
+          </div>
+        </Container>
+      </section>
       <section id="workAreas"></section>
       <section id="feedBack"></section>
-    </Container>
+    </>
   );
 };
 
