26a375d8fd0cc4dbbe1dfa77aa72859b865dfb86
Iryna Artymuk <artimyk@ukr.net>
2023-11-19 22:31:10 +0200

    feat(blog page): add blog list

--- src/pages/main/BlogPage/BlogPage.jsx
+++ src/pages/main/BlogPage/BlogPage.jsx
@@ -1,65 +1,56 @@
 import Container from '@/components/Container/Container';
-import ArrowBack from '@/components/Icons/ArrowBack';
+
 import { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import { blogData } from '@/data/blogData.js';
+import styles from './BlogPage.module.scss';
+
 const BlogPage = () => {
-  const location = useLocation();
-  console.log('location: ', location);
-  const backLinkHref = location.state?.from ?? '/';
+  // const location = useLocation();
+  // console.log('location: ', location);
+  // const backLinkHref = location.state?.from ?? '/';
   // const backLinkHref = location.pathname ?? '/';
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <Container>
-      {/* <h1>Blog</h1> */}
-      <Link to={backLinkHref}>
-        <ArrowBack />
-      </Link>
-      <ul
-        style={{
-          width: '100%',
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: 30,
-        }}
-      >
-        {blogData.map(article => (
-          <Link
-            key={article.id}
-            to={`/blog/${article.id}`}
-            state={{ from: location.pathname }}
-          >
-            <li
-              style={{
-                width: 350,
-                height: 450,
+    <section className={styles.blog}>
+      <div className={styles.blog_wrapper}>
+        <Container>
+          <h1 className="visuallyHidden">Блог Жанни Барищук</h1>
 
-                padding: 5,
-              }}
-            >
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  gap: 10,
-                }}
-              >
-                <img src={article.url} width="350" height="350" alt="" />
-                <h2> {article.title} </h2>
-              </div>
-            </li>
-          </Link>
-        ))}
-      </ul>
-    </Container>
+          <ul className={styles.blog_list}>
+            {blogData.map(article => (
+              <li key={article.id} className={styles.blog_listItem}>
+                <Link
+                  to={`/blog/${article.id}`}
+                  state={{ from: location.pathname }}
+                >
+                  <div className={styles.blog_content}>
+                    <div className={styles.blog_contentImgwrapper}>
+                      <img
+                        src={article.url}
+                        alt=""
+                        className={styles.blog_contentImg}
+                      />
+                      <div className={styles.blog_contentOverlay}>
+                        <p className={styles.blog_contentText}>
+                          {article.description}
+                        </p>
+                      </div>
+                    </div>
+
+                    <h2 className="title"> {article.title} </h2>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Container>
+      </div>
+    </section>
   );
 };
 
