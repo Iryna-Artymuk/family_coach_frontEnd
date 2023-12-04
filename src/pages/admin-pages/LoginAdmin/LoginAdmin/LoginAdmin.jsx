ba8f2f6d1bb900d6f42a26590302a3783346e4c8
Iryna Artymuk <artimyk@ukr.net>
2023-12-04 15:08:22 +0200

    feat(add admin text): add admi text

--- src/pages/admin-pages/LoginAdmin/LoginAdmin/LoginAdmin.jsx
+++ src/pages/admin-pages/LoginAdmin/LoginAdmin/LoginAdmin.jsx
@@ -1,5 +1,10 @@
 const LoginAdmin = () => {
-  return <h1>login admin</h1>;
+  return (
+    <h1 style={{ fontSize: 72, color: 'red' }}>
+      {' '}
+      you are Admin, Kyma @best
+    </h1>
+  );
 };
 
 export default LoginAdmin;
