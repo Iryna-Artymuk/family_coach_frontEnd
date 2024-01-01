9536909fb66789c9dee55964898ba3d6ea77513d
Iryna Artymuk <artimyk@ukr.net>
2024-01-01 16:31:49 +0200

    style(login page): add base layout css

--- src/components/admin/LoginLayout/LoginLayout.jsx
+++ src/components/admin/LoginLayout/LoginLayout.jsx
@@ -1,18 +1,14 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
-
 import styles from './LoginLayout.module.scss';
-import Container from '@/components/main/Container/Container';
 
 export const LoginLayout = () => {
   return (
     <div className={styles.backgroundContainer}>
-      <Container>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Outlet />
-        </Suspense>
-      </Container>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
     </div>
   );
 };
--- src/components/admin/loginPage/LoginPage.jsx
+++ src/components/admin/loginPage/LoginPage.jsx
@@ -1,13 +1,17 @@
 import Container from '@/components/main/Container/Container';
-
+import styles from './LoginPage.module.scss';
 const LoginPage = () => {
   return (
-    <Container>
-          <p className="title"> спочатку треба авторизуватись
-              сторінка в розробці, якщо вам треба доступ технічна підтримка 0666796604
-              
-          </p>
-    </Container>
+    <section>
+      <Container>
+        <div className={styles.contentWrapper}>
+          <h1 className={styles.text}>
+            спочатку треба авторизуватись сторінка знаходиться в розробці, якщо
+            вам треба доступ, технічна підтримка 0666796604
+          </h1>
+        </div>
+      </Container>
+    </section>
   );
 };
 
