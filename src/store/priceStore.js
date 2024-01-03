e9603717e309cd3c78ae203ac2a1c509b10aedb5
Iryna Artymuk <artimyk@ukr.net>
2024-01-03 10:16:59 +0200

    refactor(priceadmin): add axios to price admin

--- src/store/priceStore.js
+++ src/store/priceStore.js
@@ -1,54 +1,68 @@
 import { create } from 'zustand';
-
+import axios from '@/helpers/axios';
 const usePriceStore = create((set, get) => ({
-  // server: import.meta.env.VITE_APP_API_URL,
-  server: 'https://family-coach.onrender.com/api',
-  // server: 'http://localhost:5000/api',
   getPrices: async () => {
-    const response = await fetch(`${get().server}/price`);
-    if (!response.ok) {
+    // const response = await fetch(`${get().server}/price`);
+    // if (!response.ok) {
+    //   throw new Error(`HTTP error! Status: ${response.status}`);
+    // }
+    // const result = await response.json();
+    // return result;
+    const response = await axios.get(`/price`);
+    if (response.status !== 200) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    const result = await response.json();
-    return result;
+    return response.data;
   },
 
   addPrice: async data => {
-    const response = await fetch(`${get().server}/price`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    return result;
+    // const response = await fetch(`${get().server}/price`, {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   method: 'POST',
+    //   body: JSON.stringify(data),
+    // });
+    // const result = await response.json();
+    // return result;
+    const response = await axios.post('/price', data, {});
+    return response.data;
   },
 
   updatePrice: async (data, id) => {
-    const response = await fetch(`${get().server}/price/${id}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'PUT',
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    return result;
+    // const response = await fetch(`${get().server}/price/${id}`, {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   method: 'PUT',
+    //   body: JSON.stringify(data),
+    // });
+    // const result = await response.json();
+    // return result;
+    const response = await axios.put(`/price/${id}`, data);
+    return response.data;
   },
   deletePriceById: async (id, category) => {
     const categoryFild = {
       category: category,
     };
-    const response = await fetch(`${get().server}/price/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(categoryFild),
-    });
-    const result = await response.json();
-    return result;
+    console.log('categoryFild : ', categoryFild);
+    // const response = await fetch(`${get().server}/price/${id}`, {
+    //   method: 'DELETE',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(categoryFild),
+    // });
+    // const result = await response.json();
+    // return result;
+
+    const response = await axios.delete(
+      `/price/${id}`,
+      { data: categoryFild },
+      {}
+    );
+    return response.data;
   },
 }));
 
