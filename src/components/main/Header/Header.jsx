98b41fdcb82c784c820d696fd8f44ffb56c34d7c
Iryna Artymuk <artimyk@ukr.net>
2024-01-12 13:56:22 +0200

    style(header): fix media query

--- src/components/main/Header/Header.jsx
+++ src/components/main/Header/Header.jsx
@@ -1,37 +1,27 @@
 import { useEffect, useState } from 'react';
 
+import { useMediaQuery } from 'react-responsive';
 import Button from '../Button/Button';
 import BurgerIcon from '../../Icons/Main/BurgerIcon';
 import Logo from '../../Icons/Logo/Logo';
 import Container from '@/components/main/Container/Container';
 import BurgerMenu from './BurgerMenu';
-
-import styles from './Header.module.scss';
 import Navigation from './Navigation';
+import styles from './Header.module.scss';
 
 const Header = () => {
   const [showBurgerMenu, setShowBurgerMenu] = useState(false);
   const toggleBurgerMenu = () => setShowBurgerMenu(!showBurgerMenu);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth);
-    }
+  const isDesktop = useMediaQuery({ minWidth: 1240 });
 
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
   return (
     <header>
       <Container>
         <div className={styles.contentWrapper}>
           <Logo />
 
-          {windowWidth < 1240 ? (
+          {!isDesktop ? (
             <Button type="button" burgerButton onClick={toggleBurgerMenu}>
               <BurgerIcon />
             </Button>
