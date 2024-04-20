9edc5619633d007f9c52cc0bcd624845661485cc
Iryna Artymuk <artimyk@ukr.net>
2024-04-20 20:54:15 +0300

    fix(privat route): add token and error check

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,11 +1,19 @@
 import { useAuthorized } from '@/store/IsAuthorizedStore';
+import useAuthStore from '@/store/authStore';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children, redirectTo = 'admin/login' }) => {
   const isAuthorized = useAuthorized(state => state.isAuthorized);
-
-
-  return isAuthorized ? <>{children}</> : <Navigate to={redirectTo} replace />;
+  const { error } = useAuthStore();
+  // Get the value of the key from local storage
+  const value = localStorage.getItem('family_coach_access_token');
+  const existsToken = value !== null;
+  // Check if the value is not null
+  return isAuthorized || error || !existsToken ? (
+    <>{children}</>
+  ) : (
+    <Navigate to={redirectTo} replace />
+  );
 };
 
 export default PrivateRoute;
--- src/pages/admin-pages/AdminSharedLayout/AdminSharedLayout.jsx
+++ src/pages/admin-pages/AdminSharedLayout/AdminSharedLayout.jsx
@@ -7,32 +7,20 @@ import Sidebar from '@/components/admin/Sidebar/Sidebar';
 import AdminHeader from '@/components/admin/AdminHeader/AdminHeader';
 import useAuthStore from '@/store/authStore';
 import Spinner from '@/ui/Spinner/Spinner';
-import { useAuthorized } from '@/store/IsAuthorizedStore';
 
 const AdminSharedLayout = () => {
-  const { getCurrentUser, error } = useAuthStore();
-  const { setUnAuthorized } = useAuthorized();
+  const { getCurrentUser } = useAuthStore();
+
   const loading = useAuthStore(state => state.loading);
   const currentUser = useAuthStore(state => state.currentUser);
-
-  const navigate = useNavigate();
-  // Get the value of the key from local storage
-  const value = localStorage.getItem('family_coach_access_token');
-  const exists = value !== null;
-  // Check if the value is not null
 
   useEffect(() => {
     try {
       getCurrentUser();
-      if (!exists) {
-        setUnAuthorized();
-        <Navigate to="/admin/login" replace />;
-      }
     } catch (error) {
-      <Navigate to="/admin/login" replace />;
       console.log(error);
     }
-  }, [getCurrentUser, exists, navigate, setUnAuthorized]);
+  }, [getCurrentUser]);
   return (
     <>
       <main className={styles.mainWrapper}>
@@ -42,7 +30,7 @@ const AdminSharedLayout = () => {
           {!loading ? (
             <Suspense fallback={<div>Loading...</div>}>
               <div className={styles.rightContent}>
-                {error ? <Navigate to="/admin/login" replace /> : <Outlet />}
+                <Outlet />
               </div>
             </Suspense>
           ) : (
