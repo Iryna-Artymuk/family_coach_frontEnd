bf024eeba5fb2d31060475f1f0984e4282a889cb
Iryna Artymuk <artimyk@ukr.net>
2024-04-20 21:33:39 +0300

    fix(auth): check current user in private route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -4,13 +4,13 @@ import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children, redirectTo = 'admin/login' }) => {
   const isAuthorized = useAuthorized(state => state.isAuthorized);
-  const { error } = useAuthStore();
-  console.log(' currentUserError : ', error);
+  const currentUser = useAuthStore(state => state.currentUser);
   // Get the value of the key from local storage
   const value = localStorage.getItem('family_coach_access_token');
   const existsToken = value !== null;
-  // Check if the value is not null
-  return isAuthorized || !error || existsToken ? (
+  const existsUser = Object.keys(currentUser).length > 0;
+
+  return isAuthorized || existsUser || existsToken ? (
     <>{children}</>
   ) : (
     <Navigate to={redirectTo} replace />
